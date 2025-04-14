0dc4700337b27b57d54313251444213b13e3331f
Angad Suryavanshi <135613657+angad2212@users.noreply.github.com>
2025-04-15 00:48:37 +0530

    backend updates

--- server/index.js
+++ server/index.js
@@ -1,15 +1,27 @@
 const express = require('express');
 const cors = require('cors');
+const mongoose = require('mongoose');
+
+require("dotenv").config();
+
+
+const connectDB = async () => {
+  try {
+      const conn = await mongoose.connect(process.env.MONGO_URL);
+      console.log('Connected to MongoDB database');
+  } catch (error) {
+      console.log(`Error: ${error.message}`);
+      process.exit(1);
+  }
+};
+
 const app = express();
 const PORT = 3007;
 
 app.use(cors());
 app.use(express.json());
 
-app.get('/', (req, res) => {
-  res.send('Backend server is running!');
-});
-
 app.listen(PORT, () => {
   console.log(`Server running at http://localhost:${PORT}`);
 });
+connectDB();
